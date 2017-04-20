e1dc2336827bbca89ab894b8372db3f0715d2861
JuanRobisco <juanrobisco@dinamiq.com>
2017-04-21 00:31:13 +0200

    Many to many eloquent

--- app/Room.php
+++ app/Room.php
@@ -13,4 +13,9 @@ class Room extends Model
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
     public $timestamps = true;
+
+    public function clients()
+    {
+        return $this->belongsToMany('App\Client');
+    }
 }
