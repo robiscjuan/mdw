9c86ddbc7841a9ef8a43313a2e63302f7aa09bb8
Emilio Castellano <ecastel6@gmail.com>
2017-04-22 13:28:53 +0200

    Test routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,43 +9,32 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('inicio');
 })->name('home');
-
 Route::get('/formulario-alta', function () {
     return view('formularioAlta');
 });
-
 Route::get('/reserva-correcta', function () {
     return view('reservaOK');
 });
-
 Route::get('/reserva-errónea', function () {
     return view('reservaKO');
 });
-
 Route::get('/instalaciones', function () {
     return view('instalaciones');
 })->name('instalaciones');
-
 Route::get('/login-cliente', function () {
     return view('login');
 })->name('loginCliente');
-
 Route::get('/registro', function () {
     return view('registro');
 })->name('registro');
-
 Route::get('/reserva', function () {
     return view('reserva');
 })->name('reserva');
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index');
-
 Route::resource('booking', 'BookingController', ['only' => [
     'store'
 ]]);
